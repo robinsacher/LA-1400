e433f7e57d36512b049a1660a8f785c1bd62489e
ROBIN_SACHER <110891559+robinsacher@users.noreply.github.com>
2023-01-31 09:47:16 +0100

    Add files via upload

---
+++ Evenbetter.java
@@ -0,0 +1,67 @@
+package sacherrobin;
+import robocode.*;
+
+public class Evenbetter extends JuniorRobot
+{
+	int walls_hit = 0;
+	
+	public void standartmovement()
+	{
+		ahead(100);
+		back(25);
+		turnGunRight(360);	
+	}
+	
+	
+	public void run() 
+	{
+		setColors(red, red, red, red, red);
+		standartmovement();
+	}
+		
+	
+	public void onScannedRobot() 
+	{
+		turnGunTo(scannedAngle);
+		if(gunReady == true)
+		{
+			if(scannedDistance > 150)
+			{
+				fire(0.5);
+			}
+		
+			if (scannedDistance < 150)
+			{
+				fire(3);
+			}
+		}
+	}
+
+
+	public void onHitByBullet() 
+	{
+	    if (hitByBulletAngle != -1) 
+		{
+        	turnRight(hitByBulletAngle);     
+        	ahead(fieldHeight / 5);
+		}
+	}
+	
+
+	public void onHitWall() 
+	{
+		walls_hit =  walls_hit + 1;
+	 	if (walls_hit < 5)
+		 {
+	 		turnRight(180);
+     		ahead(fieldHeight / 5);
+		 }
+    
+		 if (walls_hit > 5)
+		 {
+			back(fieldHeight / 2);
+		 }
+	} 	
+}
+
+
